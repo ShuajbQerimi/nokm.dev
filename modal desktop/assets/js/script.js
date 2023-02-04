f3fbeea5b31ee19c72872e7bfcf2cc2e99b06cff
Shuajb Qerimi <shuajbqerimi@gmail.com>
2023-02-04 21:01:55 +0100

    fdsf

---
+++ modal desktop/assets/js/script.js
@@ -0,0 +1,140 @@
+let project11 = document.querySelector('.project11image')
+let project12 = document.querySelector('.project12image')
+let project13 = document.querySelector('.project13image')
+let project14 = document.querySelector('.project14image')
+let project15 = document.querySelector('.project15image')
+
+let project11video = document.querySelector('.project11video')
+let project12video = document.querySelector('.project12video')
+let project13video = document.querySelector('.project13video')
+let project14video = document.querySelector('.project14video')
+let project15video = document.querySelector('.project15video')
+
+
+
+let changetovideos1 = document.querySelector('.changetovideo')
+let changetoimages1 = document.querySelector('.changetoimages')
+let imagespart = document.querySelector('.imagespart')
+let videopart = document.querySelector('.videopart')
+
+
+
+
+
+
+function button1image() {
+    project11.style.display = 'block';
+    project12.style.display = 'none';
+    project13.style.display = 'none';
+    project14.style.display = 'none';
+    project15.style.display = 'none';
+
+}
+
+
+function button1image() {
+        project11.style.display = 'block';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+    
+}
+function button2image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'block';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+    
+}
+function button3image() {
+ 
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'block';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+    
+}
+function button4image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'block';
+        project15.style.display = 'none';
+    
+}
+function button5image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'block';
+}
+
+
+function button1video() {
+    project11video.style.display = 'block';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+
+}
+function button2video() {
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'block';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+
+}
+function button3video() {
+
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'block';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+
+}
+function button4video() {
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'block';
+    project15video.style.display = 'none';
+
+}
+function button5video() {
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'block';
+}
+
+
+function changetovideo() {
+    imagespart.style.display = 'none';
+    videopart.style.display = 'flex';
+    changetovideos1.style.color = '#FFFF27';
+    changetoimages1.style.color= '#FFF';
+}
+function changetoimages() {
+    imagespart.style.display = 'flex';
+    videopart.style.display = 'none';
+    changetovideos1.style.color = '#FFF';
+    changetoimages1.style.color= '#FFFF27';
+}
+
+
---
+++ nutanix-loading-screen/README.md
@@ -0,0 +1,4 @@
+# Nutanix Loading Screen
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/kenchen/pen/vYwvbZ](https://codepen.io/kenchen/pen/vYwvbZ).
+
