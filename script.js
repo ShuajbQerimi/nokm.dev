a0e849aa3ff952cc336db139fbd798be92d124d5
Shuajb Qerimi <61627401+ShuajbQerimi@users.noreply.github.com>
2025-01-07 23:03:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const phrases = [
 'We’re here to walk that path with you,',
 'delivering cutting-edge development solutions.',
 'Stay tuned for the launch of something exceptional.',
-'nokm.dev – Ideas into Reality.',
+'nokm.dev – Transforming Ideas to Reality.',
 ''
 ];
 
